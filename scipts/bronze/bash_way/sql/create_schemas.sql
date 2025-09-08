514a5643f965f51b5473f39f3679abdb770e37a4
TodMount <todmount@gmail.com>
2025-09-08 05:09:09 +0300

    refactor!: Move bronze layer bash scripts to separate folder
    - `init_db.sh` -> `create_bronze.sh`
    - `create_bronze.sh` is now main script that orchestrate whole bronze layer

--- scipts/bronze/bash_way/sql/create_schemas.sql
+++ scipts/bronze/bash_way/sql/create_schemas.sql
@@ -3,7 +3,7 @@
 Create Schemas
 =============================================================
 Simple script to create schemas according to medallion model.
-It's for use with `init_db.sh`
+It's for use with `create_bronze.sh`
 */
 CREATE SCHEMA IF NOT EXISTS bronze AUTHORIZATION :"user";
 CREATE SCHEMA IF NOT EXISTS silver AUTHORIZATION :"user";
