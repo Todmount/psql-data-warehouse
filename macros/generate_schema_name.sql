fdfd2d96827d64acddc9363d43c568417ea64788
TodMount <todmount@gmail.com>
2025-09-09 15:29:31 +0300

    feat!: Add silver level and add `bronze.crm_cst_info` to `silver.crm_customer_info` transformation
    - Details in dbt-generated docs

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,4 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {{ custom_schema_name if custom_schema_name is not none else target.schema }}
+{%- endmacro %}
+{#Thank you ChatGPT#}
