0796c1b0c4de5e8595381c1a30b2d9afa8820fb7
TodMount <todmount@gmail.com>
2025-09-09 23:18:03 +0300

    feat: Add script for generating dbt sources from raw CSV data
    - Remove redudant import from `get_project_root`
    - Add path constants

--- common/constants.py
+++ common/constants.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 
+from common.get_project_root import get_project_root
+
 load_dotenv(find_dotenv())
 
 DB_HOST = os.getenv("DB_HOST")
@@ -8,3 +10,11 @@ DB_USER = os.getenv("DB_USER")
 DB_SUPERUSER = os.getenv("DB_SUPERUSER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_SUPERPASSWORD = os.getenv("DB_SUPERPASSWORD")
+
+PROJECT_ROOT = get_project_root()
+CRM_PATH = PROJECT_ROOT / "datasets/source_crm"
+ERP_PATH = PROJECT_ROOT / "datasets/source_erp"
+MODELS_PATH = PROJECT_ROOT / "models"
+BRONZE_PATH = MODELS_PATH / "bronze"
+SILVER_PATH = MODELS_PATH / "silver"
+GOLD_PATH = MODELS_PATH / "gold"
